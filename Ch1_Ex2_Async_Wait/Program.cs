a282f75aba4ed451bf58ae73d5e8629edc611957
Pawan Raj Shakya <pawanrajshakya@Pawans-MacBook-Pro.local>
2017-11-04 07:13:20 -0400

    Ch1 Ex1 Added

---
+++ Ch1_Ex2_Async_Wait/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Ch1_Ex2_Async_Wait
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
